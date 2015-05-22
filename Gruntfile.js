56b9f69f69c98b1b965f170ba3e1c8005b3135fd
PaulGap <paulgschwendtner@me.com>
2015-05-22 16:52:58 +0200

    Grunt Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,52 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    grunt.grunt({
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-clean");
 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+
+        copy: {
+            scripts: {
+                expand: true,
+                dest: "dist/",
+                cwd: "src/",
+                src: "**/*.js"
+            }
+        },
+
+        uglify: {
+            options: {
+                mangle: false
+            },
+            scripts: {
+                files: [{
+                    expand: true,
+                    src: ["**/*.js", "!**/*.min.js"],
+                    cwd: "dist/",
+                    dest: "dist/",
+                    ext: ".min.js"
+                }]
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ["src/**/*.js"],
+                tasks: ["default"],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+
+        clean: {
+            dist: ["dist/"]
+        }
     });
+
+    grunt.registerTask("default", ["clean", "copy", "uglify"]);
 
 };
---
+++ src/sidebar.js
@@ -0,0 +1,12 @@
+"use strict"
+
+var Sidebar = function() {
+
+    this.showMessage = function(message) {
+        alert(message);
+    }
+
+};
+
+var sidebar = new Sidebar();
+sidebar.showMessage("Hello this is an API Message4eeee");
